b4f317bab651bafb9266748345b6fb086cbdb53d
Jacobo Blasco & Michael Schubert <pair+jblasco+schubert@pivotallabs.com>
2012-01-05 07:36:16 -0800

    Make the vim recipe much safer to use when using a non-pivotal repository. Also use the proper chef git resource instead of execute statements

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -25,20 +25,16 @@ execute "move existing vim_home out of the way if necessary" do
   }
 end
 
-execute "clone the .vim directory" do
+git "#{node["vim_home"]}" do
+  only_if {
+    system("test ! -d #{node["vim_home"]} || cd #{node["vim_home"]} && git remote -v | grep pivotal/vim-config")
+  }
+  repository "git@github.com:casecommons/vim-config.git"
+  branch "master"
+  revision node["vim_hash"] || "HEAD"
+  action :sync
   user WS_USER
-  command "git clone https://github.com/pivotal/vim-config.git #{node["vim_home"]}"
-  not_if "cd #{node["vim_home"]} && git remote -v | grep pivotal/vim-config"
-end
-
-execute "ensure we're on the approved hash" do
-  user WS_USER
-  command "cd #{node["vim_home"]} && git checkout #{node["vim_hash"]}"
-end
-
-execute "get the submodules" do
-  user WS_USER
-  command "cd #{node["vim_home"]} && git submodule update --init"
+  enable_submodules true
 end
 
 link "#{WS_HOME}/.vimrc" do
@@ -49,6 +45,7 @@ end
 brew_install "ctags"
 
 execute "compile command-t" do
+  only_if "test -d #{node["vim_home"]}/bundle/command-t/ruby/command-t"
   cwd "#{node["vim_home"]}/bundle/command-t/ruby/command-t"
   command "ruby extconf.rb && make"
   user WS_USER
