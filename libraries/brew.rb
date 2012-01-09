66924afc2f78fd63268b194354fb230cc58bf423
Pivotal CB <pivotal@mott.nyc.pivotallabs.com>
2012-01-09 17:52:37 -0500

    Modified brew_install to support install specific SHAs

--- libraries/brew.rb
+++ libraries/brew.rb
@@ -3,7 +3,7 @@ class Chef::Recipe
     include_recipe "pivotal_workstation::homebrew"
     sha = opts.delete("sha")
     specific_version = "https://raw.github.com/mxcl/homebrew/#{sha}/Library/Formula/#{package}.rb"
-    package = specific_version if reference
+    package = specific_version if sha
 
     prefix_command = (opts[:prefix_command]) ? "#{opts[:prefix_command]} && " : ""
     execute "brew install #{package} #{opts[:brew_args]}" do
