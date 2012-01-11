10df36dacd94474d1a5e99a041ea522033f8ce70
Pivotal Casebook <casebook-dev+github@googlegroups.com>
2012-01-11 17:37:44 -0500

    Update libraries/brew.rb

--- libraries/brew.rb
+++ libraries/brew.rb
@@ -4,10 +4,10 @@ class Chef::Recipe
     sha = opts.delete("sha")
     specific_version = "https://raw.github.com/mxcl/homebrew/#{sha}/Library/Formula/#{package}.rb"
     package = specific_version if sha
-    prefix_command = (opts[:prefix_command]) ? "#{opts[:prefix_command]} && " : ""
+    prefix_command = opts[:prefix_command]
     execute "brew install #{package} #{opts[:brew_args]}" do
       user WS_USER
-      command "#{prefix_command}brew install #{package} #{opts[:brew_args]}"
+      command "#{prefix_command} brew install #{package} #{opts[:brew_args]}"
       not_if "brew list | grep '^#{package}$'"
     end
   end
